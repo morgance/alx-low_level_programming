ebdb1fc216fb0c79febe8ebb6e57c4cca27c99f9
morgance <mogeokumu@gmail.com>
2023-02-16 10:16:48 -0800

    Hello,pus

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
  *main - entry point
  *
- *Return:0 after printing the code
+ *Return:0 after printing code
  */
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle);
-   
-   return (0);
+   puts("\"Programming is like building a multilingual puzzle");
+      return (0);
 }
