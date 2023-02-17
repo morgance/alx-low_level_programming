2c7d68caa5c475262a3d9b8e9f4c03ee9433b865
morgance <mogeokumu@gmail.com>
2023-02-17 05:57:14 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return:0 after printing code
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
