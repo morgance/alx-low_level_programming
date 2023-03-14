bb7d1c9f19a6376e3201ac3ff98bf071f2b1dc0f
morgance <mogeokumu@gmail.com>
2023-03-14 01:58:06 -0700

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory.
+ * @str: string.
+ *
+ * Return: pointer of an array of chars
+ */
+char *_strdup(char *str)
+{
+	char *stp;
+	unsigned int i, j;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++);
+
+	stp = (char *)malloc(sizeof(char) * (i + 1));
+
+	if (stp == NULL)
+		return (NULL);
+
+	for (j = 0; j <= i; j++)
+		stp[j] = str[j];
+
+	return (stp);
+}
