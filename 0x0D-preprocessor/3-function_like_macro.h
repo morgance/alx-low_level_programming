2dd61ffcaab1f1bc99184b5f1d6470bb970071fd
morgance <mogeokumu@gmail.com>
2023-03-20 12:31:37 -0700

    Function-like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,7 @@
-#ifndef  FUNCTION_LIKE_MACRO_H_
-#define  FUNCTION_LIKE_MACRO_H_
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+
 
 #endif
