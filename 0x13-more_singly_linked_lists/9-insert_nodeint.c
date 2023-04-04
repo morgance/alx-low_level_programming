3aa9247f5cdaa92fcb04055064ae348f2b077c5f
morgance <mogeokumu@gmail.com>
2023-04-04 06:52:24 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx != 0)
 	{
-		for (i = 0; i < idx - 1 && j != NULL i++)
+		for (i = 0; i < idx - 1 && j != NULL; i++)
 		{
 			j = j->next;
 		}
