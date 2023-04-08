5f4c8d6e594a0c8b69cc461630c85a32c002f99b
morgance <mogeokumu@gmail.com>
2023-04-08 03:07:15 -0700

    10

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,14 +3,13 @@
  * print_binary - prints binary representation of a number
  * @n:number in binary
  *
- * Return:nothing 
+ * Return:nothing
  */
 void print_binary(unsigned long int n)
 {
 	if (n = 0)
 	{
 		if (n >> 1)
-			print_binary(n >> 1);
 		_putchar((n & 1) + '0');
 	}
 	else
