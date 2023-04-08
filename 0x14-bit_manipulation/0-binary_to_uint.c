ac4f00df6b962034ea796091ff5a0aa5bc2e8b31
morgance <mogeokumu@gmail.com>
2023-04-08 01:06:39 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,7 +4,8 @@
  * binary_to_uint - converts binary number to unsigned int
  * @b:pointing to a string of 0 and 1 chars
  *
- * Return:The converted number or 0 if theres one or more chars in a sring of b is NULL
+ * Return:The converted number or 0
+ * if theres one or more chars in a sring of b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
@@ -15,15 +16,14 @@ unsigned int binary_to_uint(const char *b)
 		return (NULL);
 
 	i = 0;
-
 	for (prime = 0; b[prime] != '\0'; prime++)
 		;
 
-	for (prime--, rim = 1; prime >= 0; prime--, rim *=2)
+	for (prime--, rim = 1; prime >= 0; prime--, rim *= 2)
 	{
 		if (b[prim] != '0' && b[prim] != '1')
 		{
-			return (0)
+			return (0);
 		}
 
 		if (b[prim] & 1)
@@ -31,6 +31,5 @@ unsigned int binary_to_uint(const char *b)
 			i += rim;
 		}
 	}
-	
 	return (i);
 }
