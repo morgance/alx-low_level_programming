bfcba3387853d87e8e6ec20f8b6d812c3f91aff5
morgance <mogeokumu@gmail.com>
2023-04-09 03:14:00 -0700

    11

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of bit to 1 at a given index
+ * @n:the bit
+ * @index:where the bit is taken
+ *
+ * Return:1 if it works or -1 if unsuccesful 
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i;
+
+	if (index > 63)
+		return (-1);
+
+	i = 1 << index;
+	*n = (*n | m);
+
+	return (1);
+}
