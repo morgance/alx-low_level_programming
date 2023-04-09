643a406572147842ec953517cbd7b838bb4c18d1
morgance <mogeokumu@gmail.com>
2023-04-09 02:51:53 -0700

    0

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,3 @@
-#ifndef _MAIN_
-#define _MAIN_
-
-#include <stdio.h>
-#include <stdlib.h>
-
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
