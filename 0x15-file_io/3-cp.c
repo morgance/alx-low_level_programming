f5b105757c5ef4e844ec7cf316c42011c47507d1
morgance <mogeokumu@gmail.com>
2023-04-12 04:41:26 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,8 +8,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, error;
-	ssize_t prim, pro;
+	int file_from, file_to;
+	ssize_t prim, pro, error;
 	char ptr[1024];
 
 	if (argc != 3)
@@ -21,13 +21,13 @@ int main(int argc, char *argv[])
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Cant read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Cant write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	prim = 1024;
@@ -38,26 +38,26 @@ int main(int argc, char *argv[])
 
 		if (prim == -1)
 		{
-			 dprintf(STDERR_FILENO, "Error: Cant read from file %s\n", argv[1]);
+			 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			 exit(98);
 		}
 		pro = write(file_to, ptr, prim);
 		if (pro == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Cant write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
 	error = close(file_from);
 	if (error == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Cant close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	error =  close(file_to);
 	if (error == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Cant close fd %d\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 		exit(100);
 	}
 	return (0);
