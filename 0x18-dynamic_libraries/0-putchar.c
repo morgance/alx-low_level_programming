d68e0126279ba9dcfad0741d3c6e0a04f5d4ef09
morgance <mogeokumu@gmail.com>
2023-06-19 12:22:02 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return:0 after running code
+ */
+int main(void)
+{
+	char c[9] = "_putchar";
+	int i = 0;
+
+	while (i < 8)
+
+	{
+
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
+
+
+
